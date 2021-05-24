184b4b12c91b0f6d34d3683ba51e91798334a36f
Pegasoos <circumambulation24@yahoo.com>
2021-05-23 23:27:18 -0400

    html and javascript for start button

--- develop/script.js
+++ develop/script.js
@@ -4,11 +4,14 @@ var questions = document.querySelector("questions");
 
 var timer = document.getElementById('timer');
 
+var startButton = document.getElementById('start');
+
 var secondsLeft = 75;
 
 //Change Text Content on loop for questions?
 //"answer" class/id for answer buttons, group into variable, add click event
 //Timer for quiz
+//add start button
 function setTimer() {
 //window.location = "highscores.html" at end of interval(Timer that subtracts on wrong answers)
 var quizTimer = setInterval(function() {
@@ -22,8 +25,7 @@ if(secondsLeft === 0){
     window.location.assign("highscores.html");
 }
 }, 750)};
+//End of set Timer
 
-if(window.location.pathname === '/C:/Users/circu/Desktop/Code_Quiz/index.html'){
-    setTimer();
-};
+startButton.addEventListener("click", setTimer);
 //for loop to calculate score, store in variable/local storage, retrieve/display on highscores page
