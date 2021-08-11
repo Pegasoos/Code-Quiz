1603921a9735801ae4823d27b7b03eb4cad67691
Pegasoos <circumambulation24@yahoo.com>
2021-08-10 21:54:51 -0400

    completed submit funcionality

--- develop/highscore.js
+++ develop/highscore.js
@@ -1,7 +1,10 @@
 let score = localStorage.getItem("score")
 let scoreList = document.getElementById('scoreList')
 let finalScore = document.getElementById('final')
-let hiddenElements = document.getElementsByClassName("poof")
+let formElement = document.getElementById("formList")
+let hiddenButtons = document.getElementById("clearGoButtons")
+let hiddenHeader = document.getElementById('hiddenHeading')
+let pencilHeading = document.getElementById('heading')
 let initialBox = document.getElementById('initials')
 let clearButton = document.getElementById('clear')
 let submitButton = document.getElementById('submit')
@@ -16,10 +19,15 @@ let submitScore = function(e){
     li.textContent = initials + " - " + score;
     scoreList.appendChild(li);
     e.preventDefault();
+    hiddenButtons.classList.remove("poof")
+    pencilHeading.classList.add("poof")
+    finalScore.classList.add("poof")
+    formElement.classList.add("poof")
 }
 
 let clearScores = function(){
-
+    scoreList.remove("li")
 }
 
 submitButton.addEventListener("click", submitScore)
+clearButton.addEventListener("click", clearScores)
